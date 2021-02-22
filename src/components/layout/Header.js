61334d966c5886a1f1c4db635893510b068d4b96
Adam MacKinnon <mackinnon.adam@gmail.com>
2021-02-22 17:03:22 -0500

    updated design, blog page

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../../assets/styles/styles.css'
+import Navbar from './navbar'   
 
 
 const Header = () => {
@@ -12,28 +13,8 @@ const Header = () => {
         <img className="mainlogo" src="images/primarylogoblack.jpg" height="200px;" alt=""/>
       </div>
     </div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <Link className="navbar-brand" to="#"><img height="40px" src="images/heartnotext.jpg" alt="" /></Link>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse" id="navbarNav">
-    <ul className="navbar-nav ml-auto">
-      <li className="nav-item active">
-        <Link className="nav-link" to="https://rabsurvivalgame.web.app/">RAB Survival Game <span className="sr-only">(current)</span></Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/about_us">About Us</Link>
-      </li>
-      {/* <li className="nav-item">
-        <Link className="nav-link" to="/classes">Classes</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link disabled" to="#">Disabled</Link>
-      </li> */}
-    </ul>
-  </div>
-</nav>
+    <Navbar />
+
     </>
   )
 }

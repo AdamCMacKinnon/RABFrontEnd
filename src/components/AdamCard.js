09b3d566aa77ebb2b770b1c44207fe714ab32cd7
Adam MacKinnon <mackinnon.adam@gmail.com>
2021-02-24 15:53:05 -0500

    updated personal card, styling

--- src/components/AdamCard.js
+++ src/components/AdamCard.js
@@ -5,19 +5,15 @@ const AdamCard = () => {
   return (
     <>
     < AboutUs />
-      <div className="cardBio col-6 offset-3">
+    <div className="card mb-3 col-8 offset-2">
+  {/* <h5 className="card-title">Podcast Feed</h5> */}
   <img className="card-img-top" src="images/picture.jpg" alt="" />
   <div className="card-body">
-    <h5 className="cardBio-text">Adam C. MacKinnon</h5>
-    <p className="cardBio-text">Founder, Senior Writer</p>
-    <p className="cardBio-text">Contributor:</p>
-    <p className="cardBio-text">"Call To The Pen"</p>
-    <img className="adamcardimage" src="images/cttplogo.jpeg" alt=""/>
-    <p className="cardBio-text">"Baseball Almanac"</p>
-    <img className="adamcardimage" src="images/bbalmanaclogo.jpeg" alt=""/>
-    <p className="cardBio-text">Author: "Baseball For Kids: A Young Fan's Guide to the History of the Game"</p>
-    <img className="adamcardimagebbfk" src="images/bbfk.jpg" alt=""/>
-
+    <h5 className="card-text">Adam C. MacKinnon</h5>
+    <p className="card-text"><small className="text-muted">Founder, Senior Writer</small></p>
+    <p className="card-text">An avid Braves fan, but lover of the game wherever it's played.  Contributing writer for Call to the Pen and Baseball Almanac. 
+    His first book "Baseball for Kids: A Young Fan's Guide to the History of the Game" is out now.</p>
+    
   </div>
 </div>
     </>
